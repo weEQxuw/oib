44e5e5e3787fc237358d531fd24b14e18cb59ea7
natal <natal@CLOWN>
2024-05-21 17:56:27 +0400

    added paths from the user

--- lab_3/main.py
+++ lab_3/main.py
@@ -18,29 +18,56 @@ def main() -> None:
                        help='Запускает режим дешифрования', action="store_true")
     parser.add_argument('-p', '--paths', type=str, help="Путь до json файла")
 
+    parser.add_argument('--sym-key-path', type=str,
+                        help="Путь для симметричного ключа")
+    parser.add_argument('--priv-key-path', type=str,
+                        help="Путь для приватного ключа")
+    parser.add_argument('--pub-key-path', type=str,
+                        help="Путь для публичного ключа")
+    parser.add_argument('--input-file', type=str,
+                        help="Путь к исходному файлу")
+    parser.add_argument('--decr-file', type=str,
+                        help="Путь к дешифрованному файлу файлу")
+    parser.add_argument('--encr-file', type=str,
+                        help="Путь к шифрованному файлу файлу")
+
     args = parser.parse_args()
 
-    paths = FileHelper.read_json_dict(args.paths)
+    if args.paths:
+        paths = FileHelper.read_json_dict(args.paths)
+
+    sym_key_path = args.sym_key_path if args.sym_key_path else paths.get(
+        'symmetric_key')
+    private_key_path = args.priv_key_path if args.priv_key_path else paths.get(
+        'secret_key')
+    public_key_path = args.pub_key_path if args.pub_key_path else paths.get(
+        'public_key')
+    initial_file = args.input_file if args.input_file else paths.get(
+        'initial_file')
+    encrypted_file = args.encr_file if args.encr_file else paths.get(
+        'encrypted_file')
+    decrypted_file = args.decr_file if args.encryption else paths.get(
+        'decrypted_file')
 
     match(args.generation, args.encryption, args.decryption):
         case(True, False, False):
             sym_key, private_key, public_key = CryptoSystem.generate_key()
-            FileHelper.write_bytes(paths["symmetric_key"], sym_key)
-            FileHelper.write_private_key(paths["secret_key"], private_key)
-            FileHelper.write_public_key(paths["public_key"], public_key)
+            FileHelper.write_bytes(sym_key_path, sym_key)
+            FileHelper.write_private_key(private_key_path, private_key)
+            FileHelper.write_public_key(public_key_path, public_key)
         case(False, True, False):
-            text = FileHelper.read_file(paths["initial_file"])
-            sym_key = FileHelper.read_bytes(paths['symmetric_key'])
-            private_key = FileHelper.read_private_key(paths['secret_key'])
+            text = FileHelper.read_file(initial_file)
+            sym_key = FileHelper.read_bytes(sym_key_path)
+            private_key = FileHelper.read_private_key(private_key_path)
             encrypted_text = CryptoSystem.encrypt(text, sym_key, private_key)
-            FileHelper.write_bytes(paths["encrypted_file"], encrypted_text)
+            FileHelper.write_bytes(encrypted_file, encrypted_text)
         case(False, False, True):
-            encrypted_text = FileHelper.read_bytes(paths["encrypted_file"])
-            sym_key = FileHelper.read_bytes(paths['symmetric_key'])
-            private_key = FileHelper.read_private_key(paths['secret_key'])
+            encrypted_text = FileHelper.read_bytes(encrypted_file)
+            sym_key = FileHelper.read_bytes(sym_key_path)
+            private_key = FileHelper.read_private_key(private_key_path)
             decrypted_text = CryptoSystem.decrypt(
                 encrypted_text, sym_key, private_key)
-            FileHelper.write_file(decrypted_text, paths["decrypted_file"])
+            FileHelper.write_file(decrypted_text, decrypted_file)
 
 
 if __name__ == "__main__":
